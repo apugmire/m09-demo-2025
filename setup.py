40e8a704a46f9be31666b4ee349468797cd72d34
Alysa Pugmire <alysapugmire@gmail.com>
2025-03-18 20:18:37 -0400

    First Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/apugmire/m09-demo-2025',
+    author='A. Pugmire',
+    author_email='amp3xs@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
